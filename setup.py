c472699700c22de16c83fef0fd08e136a6443da0
Omar Elsherif <omarelsherif010@gmail.com>
2022-08-19 07:38:11 +0200

    adding init, setup and test object of dnaplotlib2

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='DNAplotlib2',
-    version='0.2.0',
-    packages=find_packages(include=['dnaplotlib2', 'dnaplotlib2.*']),
-    tests_require=['pytest'],
+    version='2.0',
+    description = 'Genetic design visualization',
+    packages= ['dnaplotlib2', 'dnaplotlib2.*'],
+    tests_require = ['pytest']
 )
