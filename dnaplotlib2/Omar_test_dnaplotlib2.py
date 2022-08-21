62b83e664a704e7afed693a849e58dbc2387abe4
Omar Elsherif <omarelsherif010@gmail.com>
2022-08-21 13:37:15 +0200

    Update Omar_test_dnaplotlib2.py

--- dnaplotlib2/Omar_test_dnaplotlib2.py
+++ dnaplotlib2/Omar_test_dnaplotlib2.py
@@ -1,25 +1,25 @@
-from dnaplotlib2 import BioDesign, Backbone, Part, MolecularSpecies, Interaction, InteractionNode
+import dnaplotlib2 as dpl
 
 ## First Backbone
 ##############################################################
-b1_part1 = Part('Promoter1',1,'SO:2253254236',"Omar's Promoter")
+b1_part1 = dpl.Part('Promoter1',1,'SO:2253254236',"Omar's Promoter")
 b1_part1_dict = b1_part1.part_di()
 # print(part1_dic)
 
-b1_part2 = Part('Promoter2',1,'SO:432436',"Tom's Promoter")
+b1_part2 = dpl.Part('Promoter2',1,'SO:432436',"Tom's Promoter")
 b1_part2_dict = b1_part2.part_di()
 
-b1_part3 = Part('RDS',1,'SO:923659328567',"Ahmed's RBS")
+b1_part3 = dpl.Part('RDS',1,'SO:923659328567',"Ahmed's RBS")
 b1_part3_dict = b1_part3.part_di()
 # print(part3_dict)
 
-b1_part4 = Part('CDS',1,'SO:87265923869328567',"James's CDS")
+b1_part4 = dpl.Part('CDS',1,'SO:87265923869328567',"James's CDS")
 b1_part4_dict = b1_part4.part_di()
 
-b1_part5 = Part('Terminator',1,'SO:87265923869328567',"Tom's Terminator")
+b1_part5 = dpl.Part('Terminator',1,'SO:87265923869328567',"Tom's Terminator")
 b1_part5_dict = b1_part5.part_di()
 
-backbone1 = Backbone(name="Backbone 1")
+backbone1 = dpl.Backbone(name="Backbone 1")
 backbone1.append_part(b1_part1_dict)
 backbone1.append_part(b1_part2_dict)
 backbone1.append_part(b1_part3_dict)
@@ -28,24 +28,24 @@ backbone1.append_part(b1_part5_dict)
 
 ## Second Backbone
 ############################################################ 
-b2_part1 = Part('b2_Promoter1',1,'SO:2253254236',"Omar's Promoter")
+b2_part1 = dpl.Part('b2_Promoter1',1,'SO:2253254236',"Omar's Promoter")
 b2_part1_dict = b2_part1.part_di()
 # print(part1_dic)
 
-b2_part2 = Part('b2_Promoter2',1,'SO:432436',"Tom's Promoter")
+b2_part2 = dpl.Part('b2_Promoter2',1,'SO:432436',"Tom's Promoter")
 b2_part2_dict = b2_part2.part_di()
 
-b2_part3 = Part('b2_RDS',1,'SO:923659328567',"Ahmed's RBS")
+b2_part3 = dpl.Part('b2_RDS',1,'SO:923659328567',"Ahmed's RBS")
 b2_part3_dict = b2_part3.part_di()
 # print(part3_dict)
 
-b2_part4 = Part('b2_CDS',1,'SO:87265923869328567',"James's CDS")
+b2_part4 = dpl.Part('b2_CDS',1,'SO:87265923869328567',"James's CDS")
 b2_part4_dict = b2_part4.part_di()
 
-b2_part5 = Part('b2_Terminator',1,'SO:87265923869328567',"Tom's Terminator")
+b2_part5 = dpl.Part('b2_Terminator',1,'SO:87265923869328567',"Tom's Terminator")
 b2_part5_dict = b2_part5.part_di()
 
-backbone2 = Backbone(name="Backbone 2")
+backbone2 = dpl.Backbone(name="Backbone 2")
 backbone2.append_part(b2_part1_dict)
 backbone2.append_part(b2_part2_dict)
 backbone2.append_part(b2_part3_dict)
@@ -55,35 +55,35 @@ backbone2.append_part(b2_part5_dict)
 
 ## Molecular Species
 ##################################
-mo1 = MolecularSpecies('small molecule', 'SO:235396', 'First Molecule')
+mo1 = dpl.MolecularSpecies('small molecule', 'SO:235396', 'First Molecule')
 mo1.molec_spec_append()
 
-mo2 = MolecularSpecies('macromolecule', 'SO:234455396', 'Second Molecule')
+mo2 = dpl.MolecularSpecies('macromolecule', 'SO:234455396', 'Second Molecule')
 mo2.molec_spec_append()
 
 ## Interaction Node
 ############################################
-node1 = InteractionNode('dissociation', 'SO:1858478', 'First Interaction Node')
+node1 = dpl.InteractionNode('dissociation', 'SO:1858478', 'First Interaction Node')
 node1.node_dict()
 
 ## Interaction between b1 CDS and b2 RBS
 ########################################
-inter1 = Interaction('control', 'b2_RBS', 'b1_CDS', 'first interaction')
+inter1 = dpl.Interaction('control', 'b2_RBS', 'b1_CDS', 'first interaction')
 inter1.interaction_append()
 
-inter2 = Interaction('control', 'b1_CDS', 'node1' , 'second interaction')
+inter2 = dpl.Interaction('control', 'b1_CDS', 'node1' , 'second interaction')
 inter2.interaction_append()
 
-inter3 = Interaction('control', 'mo2', 'node1' , 'third interaction')
+inter3 = dpl.Interaction('control', 'mo2', 'node1' , 'third interaction')
 inter3.interaction_append()
 
-inter4 = Interaction('control', 'node1', 'mo1' , 'forth interaction')
+inter4 = dpl.Interaction('control', 'node1', 'mo1' , 'forth interaction')
 inter4.interaction_append()
 
 ## Bio Design
 ##################################################
 
-Bio1 =BioDesign("First BioDesign")
+Bio1 = dpl.BioDesign("First BioDesign")
 Bio1.add_backbone(backbone1.return_backbone())
 Bio1.add_backbone(backbone2.return_backbone())
 
