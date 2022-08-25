1e888ae27eacd33a77623162199cf690d44c2e31
Omar Elsherif <omarelsherif010@gmail.com>
2022-08-25 10:07:23 +0200

    Update backbone_test.py

--- tests/backbone_test.py
+++ tests/backbone_test.py
@@ -6,10 +6,10 @@ import dnaplotlib2 as dpl
 
 def test_backbone_append():
     '''
-    functiont o test return function of backbone class
+    functiont to test return function of backbone class
     '''
     part1 = dpl.Part('p','1','123','q')
     backbone1 = dpl.Backbone(name='test1')
     backbone1.append_part(part1.part_li())
-
-    assert backbone1.return_backbone() == ['test1', [['p','1','123','q']]]
+    output = str(['test1', [['p','1','123','q']]])
+    assert backbone1 == output
