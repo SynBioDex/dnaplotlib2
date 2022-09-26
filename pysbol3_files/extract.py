d0fc58e16b77026db2633526437094bbd02580ba
Omar Elsherif <omarelsherif010@gmail.com>
2022-09-26 17:19:32 +0200

    add SO to glyphtype dictionary

--- pysbol3_files/extract.py
+++ pysbol3_files/extract.py
@@ -3,6 +3,11 @@ import sbol3
 
 doc = sbol3.Document()
 doc.read("pysbol3_files/distribution.nt")
+
+SO_to_glyph_type = {'0000031': 'Aptamer', '0001953':'Assembly Scar', '0001691':'Blunt Restriction Site', 
+                    '0000316':'CDS', '0000830': "Chromosome Part 3' End",'0000830':"Chromosome Part 3' End",
+                    '0002211':"Circular Plasmid 3' end", '0002211':"Circular Plasmid 5' end", '':"Spacer",
+                    '0001688':"DNA Cleavage Site", '0001687':"DNA Cleavage Site",}
 i = 0
 objects_list = []
 backbone_list = []
@@ -13,8 +18,10 @@ for object in doc.objects:
 
     for feature in object.features:
         # print(f"Name: {feature.name}, roles: {feature.roles}, orientation: {feature.orientation}")
+        SO_term = feature.roles[0][feature.roles[0].find("SO:")+3 : ]
+        glyph_type = SO_to_glyph_type[SO_term]
 
-        part1 = dpl.Part(part_type=' ', name= feature.name, so_term= feature.roles, orientation= feature.orientation)
+        part1 = dpl.Part(part_type= glyph_type , name= feature.name, so_term= feature.roles, orientation= feature.orientation)
         part1_dict = part1.part_di()
         # print(part1_dict)
 
