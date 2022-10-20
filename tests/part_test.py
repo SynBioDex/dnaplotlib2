9de20bde20a72b6cc7895be24e23ef3cfedfadf4
Omar Elsherif <omarelsherif010@gmail.com>
2022-10-20 10:11:27 +0200

    Update part_test.py

--- tests/part_test.py
+++ tests/part_test.py
@@ -8,4 +8,4 @@ def test_part_list():
     function to test the return of part list function
     '''
     part_1 = dpl.Part('o',1, "SO:1", 'm')
-    assert part_1.part_li() == ['o',1, "SO:1", 'm']
+    assert part_1.part_li() == ['o',1, "SO:1", 'm', (0, 0)]
