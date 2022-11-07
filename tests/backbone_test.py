d994b8c0f6bf66ad005bd1b0c79e91c2af465082
Omar Elsherif <omarelsherif010@gmail.com>
2022-11-07 06:06:47 +0200

    update test files

--- tests/backbone_test.py
+++ tests/backbone_test.py
@@ -1,15 +1,102 @@
-'''
+"""
 test case for backbone class functions
-'''
+"""
 
-import dnaplotlib2 as dpl 
+import dnaplotlib2 as dpl
+
 
 def test_backbone_append():
-    '''
+    """
     functiont to test return function of backbone class
-    '''
-    part1 = dpl.Part('p','1','123','q')
-    backbone1 = dpl.Backbone(name='test1')
-    backbone1.append_part(part1.part_li())
-    output = ['test1', [['p','1','123','q', (0, 0)]]]
-    assert backbone1.backbones_list == output
+    """
+    part_1 = dpl.Part(
+        part_type="Promoter",
+        orientation=None,
+        so_term=["https://identifiers.org/SO:0000167"],
+        name="Promoter 1",
+        start_seq_range=1,
+        end_seq_range=None,
+    )
+
+    part_2 = dpl.Part(
+        part_type="Promoter",
+        orientation=None,
+        so_term=["https://identifiers.org/SO:0000167"],
+        name="Promoter 2",
+        start_seq_range=5,
+        end_seq_range=None,
+    )
+
+    part_3 = dpl.Part(
+        part_type="Promoter",
+        orientation=None,
+        so_term=["https://identifiers.org/SO:0000167"],
+        name="Promoter 3",
+        start_seq_range=10,
+        end_seq_range=None,
+    )
+
+    part_4 = dpl.Part(
+        part_type="Promoter",
+        orientation=None,
+        so_term=["https://identifiers.org/SO:0000167"],
+        name="Promoter 4",
+        start_seq_range= 15,
+        end_seq_range=None,
+    )
+
+    backbone1 = dpl.Backbone(name="test1")
+    backbone1.append_part(part_1.part_di())
+    backbone1.append_part(part_2.part_di())
+    backbone1.append_part(part_3.part_di())
+    backbone1.append_part(part_4.part_di())
+
+    output = [
+        {
+            "part_type": "Promoter",
+            "orientation": None,
+            "so_term": ["https://identifiers.org/SO:0000167"],
+            "name": "Promoter 1",
+            "start_point": (0, 0),
+            "start_seq_range": 1,
+            "end_seq_range": None,
+        },
+        {
+            "part_type": "Promoter",
+            "orientation": None,
+            "so_term": ["https://identifiers.org/SO:0000167"],
+            "name": "Promoter 2",
+            "start_point": (0, 0),
+            "start_seq_range": 5,
+            "end_seq_range": None,
+        },
+        {
+            "part_type": "Promoter",
+            "orientation": None,
+            "so_term": ["https://identifiers.org/SO:0000167"],
+            "name": "Promoter 3",
+            "start_point": (0, 0),
+            "start_seq_range": 10,
+            "end_seq_range": None,
+        },
+        {
+            "part_type": "Promoter",
+            "orientation": None,
+            "so_term": ["https://identifiers.org/SO:0000167"],
+            "name": "Promoter 4",
+            "start_seq_range": 15,
+            "start_point": (0, 0),
+            "end_seq_range": None,
+        },
+    ]
+
+
+    correct_tests = 0
+    # iterate over backbone parts and output parts 
+    for b_dict, out_dict in zip(backbone1.backbones, output):
+        # iterate over parts in backbone and use same dictionary key to compare values with output dictionary
+        for key in b_dict:
+            if b_dict[key] == out_dict[key]:
+                correct_tests += 1
+
+    assert correct_tests == len(backbone1.backbones) * len(backbone1.backbones[0])
--- tests/biodesign_test.py
+++ tests/biodesign_test.py
@@ -1,5 +1,5 @@
 '''
-test case to for biodesign 
+test case for biodesign 
 '''
 
 import dnaplotlib2 as dpl
@@ -9,11 +9,101 @@ def test_biodesign():
     funtion to test return of biodesign and make sure every element have been added
     '''
 
-    bio_1 =  dpl.BioDesign('First BioDesign')
-    bio_1.add_backbone('Backbone1')
-    bio_1.add_moleculer('Moleculer1')
-    bio_1.add_interaction_node('Node1')
-    bio_1.add_interaction('Interaction')
-    bio_1.show_biodesign()
+    part_1 = dpl.Part(
+        part_type="Promoter",
+        orientation=None,
+        so_term=["https://identifiers.org/SO:0000167"],
+        name="Promoter 1",
+        start_seq_range=1,
+        end_seq_range=None,
+    )
 
-    assert bio_1.biodesign_output == ['First BioDesign',['Backbone1'], ['Moleculer1'],['Node1'],['Interaction']]
+    part_2 = dpl.Part(
+        part_type="Promoter",
+        orientation=None,
+        so_term=["https://identifiers.org/SO:0000167"],
+        name="Promoter 2",
+        start_seq_range=5,
+        end_seq_range=None,
+    )
+
+    backbone1 = dpl.Backbone(name="backbone 1")
+    backbone1.append_part(part_1.part_di())
+    backbone1.append_part(part_2.part_di())
+
+
+    part_3 = dpl.Part(
+        part_type="Promoter",
+        orientation=None,
+        so_term=["https://identifiers.org/SO:0000167"],
+        name="Promoter 3",
+        start_seq_range=10,
+        end_seq_range=None,
+    )
+
+    part_4 = dpl.Part(
+        part_type="Promoter",
+        orientation=None,
+        so_term=["https://identifiers.org/SO:0000167"],
+        name="Promoter 4",
+        start_seq_range= 15,
+        end_seq_range=None,
+    )
+    backbone2 = dpl.Backbone(name="backbone 2")
+    backbone2.append_part(part_3.part_di())
+    backbone2.append_part(part_4.part_di())
+
+    bio_1 =  dpl.BioDesign('First BioDesign')
+    bio_1.add_backbone(backbone1)
+    bio_1.add_backbone(backbone2)
+    
+
+    output = [[
+        {
+            "part_type": "Promoter",
+            "orientation": None,
+            "so_term": ["https://identifiers.org/SO:0000167"],
+            "name": "Promoter 1",
+            "start_point": (0, 0),
+            "start_seq_range": 1,
+            "end_seq_range": None,
+        },
+        {
+            "part_type": "Promoter",
+            "orientation": None,
+            "so_term": ["https://identifiers.org/SO:0000167"],
+            "name": "Promoter 2",
+            "start_point": (0, 0),
+            "start_seq_range": 5,
+            "end_seq_range": None,
+        }],
+        [{
+            "part_type": "Promoter",
+            "orientation": None,
+            "so_term": ["https://identifiers.org/SO:0000167"],
+            "name": "Promoter 3",
+            "start_point": (0, 0),
+            "start_seq_range": 10,
+            "end_seq_range": None,
+        },
+        {
+            "part_type": "Promoter",
+            "orientation": None,
+            "so_term": ["https://identifiers.org/SO:0000167"],
+            "name": "Promoter 4",
+            "start_seq_range": 15,
+            "start_point": (0, 0),
+            "end_seq_range": None,
+        }]
+    ]
+
+    output_backbones_size = 0
+    correct_tests = 0
+    for back_list, out_list in zip(bio_1.backbones, output):
+        output_backbones_size += len(out_list)
+        for part_dict, out_dict in zip(back_list.backbones, out_list):
+            for key in part_dict:
+                if part_dict[key] == out_dict[key]:
+                    correct_tests += 1
+
+    assert correct_tests == output_backbones_size * len(output[0][0])
