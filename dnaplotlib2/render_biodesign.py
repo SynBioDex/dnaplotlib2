8c00ce67593355201bc8b618c8105dc13b59146f
Omar Elsherif <omarelsherif010@gmail.com>
2022-11-07 06:05:58 +0200

    edit name of attributes in parts

--- dnaplotlib2/render_biodesign.py
+++ dnaplotlib2/render_biodesign.py
@@ -2,19 +2,19 @@ import dnaplotlib2 as dpl
 
 
 b1_part2 = dpl.Part(part_type="Promoter", orientation = None, 
-                        so_term=['https://identifiers.org/SO:0000167'], name='Promoter 1', start_range=1)
+                        so_term=['https://identifiers.org/SO:0000167'], name='Promoter 1', start_seq_range=1)
 b1_part2_dict = b1_part2.part_di()
 
 b1_part3 = dpl.Part(part_type="Recombination Site", orientation=None ,
-                         so_term=['https://identifiers.org/SO:0000139'], name='RBS 1', start_range=6)
+                         so_term=['https://identifiers.org/SO:0000139'], name='RBS 1', start_seq_range=6)
 b1_part3_dict = b1_part3.part_di()
 
 b1_part4 = dpl.Part(part_type="CDS", orientation=None , 
-                        so_term=['https://identifiers.org/SO:0000316'], name='GFP 1', start_range=3)
+                        so_term=['https://identifiers.org/SO:0000316'], name='GFP 1', start_seq_range=3)
 b1_part4_dict = b1_part4.part_di()
 
 b1_part5 = dpl.Part(part_type="Terminator", orientation=None , 
-                        so_term=['https://identifiers.org/SO:0000141'], name='Terminator 1', start_range=4)
+                        so_term=['https://identifiers.org/SO:0000141'], name='Terminator 1', start_seq_range=4)
 b1_part5_dict = b1_part5.part_di()
 
 
@@ -34,24 +34,23 @@ backbone1.append_part(b1_part5_dict)
 
 ## Second Backbone
 
-b2_part2 = dpl.Part(part_type="Promoter", orientation = None,
-                             so_term=['https://identifiers.org/SO:0000167'], name='Promoter 2', start_range=1)
+b2_part2 = dpl.Part(part_type="Promoter", orientation = None, 
+                            so_term=['https://identifiers.org/SO:0000167'], name='Promoter 2', start_seq_range=1)
 b2_part2_dict = b2_part2.part_di()
 
 b2_part3 = dpl.Part(part_type="Recombination Site", orientation=None , 
-                            so_term=['https://identifiers.org/SO:0000139'], name='RBS 2', start_range=2)
+                            so_term=['https://identifiers.org/SO:0000139'], name='RBS 2', start_seq_range=2)
 b2_part3_dict = b2_part3.part_di()
 
 
 b2_part5 = dpl.Part(part_type="Terminator", orientation=None , 
-                            so_term=['https://identifiers.org/SO:0000141'], name='Terminator 2', start_range=3)
+                            so_term=['https://identifiers.org/SO:0000141'], name='Terminator 2', start_seq_range=3)
 b2_part5_dict = b2_part5.part_di()
 
 backbone2 = dpl.Backbone(name="Backbone 2")
 backbone2.append_part(b2_part2_dict)
 backbone2.append_part(b2_part3_dict)
 backbone2.append_part(b2_part5_dict)
-
 
 
 # inter2 = dpl.Interaction(interaction_type="control", start_object=backbone2.backbones[1], end_object=backbone2.backbones[2], 
