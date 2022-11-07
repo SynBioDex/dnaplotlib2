902c70de07944794aacdc6f403d14c3c33950f92
Omar Elsherif <omarelsherif010@gmail.com>
2022-11-07 18:24:44 +0200

    delete __init__.py of parasbolv and leaving the one in dnaplotlib2

--- dnaplotlib2/__init__.py
+++ dnaplotlib2/__init__.py
@@ -1,11 +1,11 @@
 """
 DNAplotlib2
 """
-from __future__ import division, print_function
+# from __future__ import division, print_function
 
 from .dnaplotlib2 import *
 
 # """
 # paraSBOLv
 # """
-from .parasbolv import *
+# from .parasbolv import *
--- dnaplotlib2/parasbolv/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-paraSBOLv
-"""
-from __future__ import division, print_function
-from .parasbolv import *
