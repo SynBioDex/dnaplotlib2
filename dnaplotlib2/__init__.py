aec39afe37c3c40ae711ad73d491d6f964735244
Omar Elsherif <omarelsherif010@gmail.com>
2022-11-07 09:08:40 +0200

    add import parasbolv to __init__.py of dnaplotlib2

--- dnaplotlib2/__init__.py
+++ dnaplotlib2/__init__.py
@@ -7,5 +7,5 @@ from .dnaplotlib2 import *
 # """
 # paraSBOLv
 # """
-# from __future__ import division, print_function
-# from .parasbolv.parasbolv import *
+from __future__ import division, print_function
+from .parasbolv.parasbolv import *
